de6d6b73d591c6333ba7c7083ecfd6e9605ea055
Kuklyshyn <kuklishinnick@gmail.com>
2021-05-21 15:29:08 +0300

    Google Map

---
+++ components/Map.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <GMap
+      ref="gMap"
+      language="en"
+      :center="{lat: locations[0].lat, lng: locations[0].lng}"
+      :zoom="10"
+    >
+      <GMapMarker
+        v-for="location in locations"
+        :key="location.id"
+        :position="{lat: location.lat, lng: location.lng}"
+        :options="{icon: location.logo}"
+
+      >
+        <GMapInfoWindow :options="{maxWidth: 200}">
+          <code>
+            lat: {{ location.lat }},
+            lng: {{ location.lng }}
+          </code>
+        </GMapInfoWindow>
+      </GMapMarker>
+    </GMap>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Map",
+  props: {
+    locations: {
+      type: Array,
+      required: true
+    }
+  },
+  data: () => ({
+    currentLocation: {}
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <div>
+      <h1>Map</h1>
+    </div>
+    <Map :locations="locations" v-if="locations.length > 0"></Map>
+  </div>
+</template>
+
+<script>
+import Map from "~/components/Map";
+export default {
+  name: "about",
+  components: {
+    Map
+  },
+  data: () => ({
+    locations: []
+  }),
+  async mounted() {
+    const result = await this.$axios.$get('https://experts.sirv.com/backend/wp-json/api/v2/experts');
+    this.locations = [... JSON.parse(result).filter(e => e.status === 'publish')];
+    console.log(this.locations[0].logo);
+  }
+}
+</script>
+
+<style scoped>
+li {
+  margin-bottom: 40px;
+}
+img {
+  max-width: 50px;
+  height: 50px;
+}
+
+span {
+  display: block;
+}
+</style>
